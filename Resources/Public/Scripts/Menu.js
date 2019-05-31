b00ddd9fe70197783ff6de3ea9106b9cb10086c5
Maciej <maciej@myx.systems>
2019-05-31 16:07:34 +1000

    menu

---
+++ Resources/Public/Scripts/Menu.js
@@ -0,0 +1,9 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+    document.getElementById("skyLaunchNav").style.height = "100%";
+}
+
+/* Close when someone clicks on the "\" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("skyLaunchNav").style.height = "0%";
+}
